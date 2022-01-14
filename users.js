50cb0c2de53d714f9a928fb46c9c4367b18bd4cf
Eleazar <jai690@hotmail.com>
2022-01-13 23:21:40 -0600

    setear interaccion db y recibir body

--- users.js
+++ users.js
@@ -1,24 +1,35 @@
+const AWS = require('aws-sdk');
+
 module.exports = {
 
   signup: async (event) => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify(
-        {
-          message: "Signup successfully!",
-        },
-        null,
-        2
-      ),
+    
+    const dynamodb = new AWS.DynamoDB.DocumentClient();
+    const id = String(Math.random()).slice(2,7)
+    const params = {
+      Item: {
+        'UserId': id,
+        'name' : 'brayan',
+        'email' : 'jai690@hotmail.com',
+        'password' : 'ejemplo'
+      },
+      TableName: 'users',
     }
+
+    return dynamodb.put(params).promise().then(()=>{
+      return id;
+    });
   },
 
   signin: async (event) => {
+    const { email } = JSON.parse(event.body);
+
     return {
       statusCode: 200,
       body: JSON.stringify(
         {
           message: "Signin successfully!",
+          email
         },
         null,
         2
